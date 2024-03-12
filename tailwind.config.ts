86e60dbba9a8b3b25c1e44875d8d89c4cc8674e1
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-03-12 14:50:34 -0300

    chore: tailwind setup

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+        certification: '45% 10% 45%',
+      },
+
+      colors: {
+        github: '#171515',
+        skills: '#7C3AED',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
