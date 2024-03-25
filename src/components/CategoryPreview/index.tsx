45fd0b15c8aba6c3867909bc8719c166912eefbe
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-03-25 19:15:57 -0300

    feat: add the category name on category page

--- src/components/CategoryPreview/index.tsx
+++ src/components/CategoryPreview/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { ProductCard } from '../ProductCard'
 import './styles.scss'
 
@@ -15,7 +16,9 @@ export function CategoryPreview({ title, products }: CategoryPreviewProps) {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link to={title} className="title">
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products
--- src/routes/Category/index.tsx
+++ src/routes/Category/index.tsx
@@ -7,18 +7,21 @@ import { ProductCard } from '../../components/ProductCard'
 export function Category() {
   const { categories } = useCategories()
   const { category } = useParams()
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState(categories[category])
 
   useEffect(() => {
     setProducts(categories[category])
   }, [category, categories])
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <>
+      <h2 className="category-title">{category?.toLocaleUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
   )
 }
